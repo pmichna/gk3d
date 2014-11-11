7e495c2fa527cc3aa0e0e6589df2f33ca35d6f50
Pawel Michna <pmichna@gmail.com>
2014-11-11 23:53:59 +0100

    added 2nd bench + refactoring

--- gk3d/gk3d/Arena.cs
+++ gk3d/gk3d/Arena.cs
@@ -6,22 +6,115 @@ namespace gk3d
 {
     class Arena : Cuboid
     {
-        public Cuboid LeftPost { get; private set; }
-        public Cuboid RightPost { get; private set; }
-        public Cuboid Net { get; private set; }
-        public Model Bench1;
-        private Model bench2;
-        private ContentManager _content;
+        private Cuboid LeftPost { get; set; }
+        private Cuboid RightPost { get; set; }
+        private Cuboid Net { get; set; }
+        private readonly Model _bench;
+        private readonly BasicEffect _effect;
+        private readonly GraphicsDevice _graphicsDevice;
 
-        public Arena(ContentManager content, Vector3 center, int width, int height, int depth, Color color)
+        public Arena(ContentManager content, GraphicsDevice device, Vector3 center, int width, int height, int depth, Color color)
             : base(center, width, height, depth, true, color)
         {
-            _content = content;
+            _graphicsDevice = device;
+            _effect = new BasicEffect(_graphicsDevice);
             LeftPost = new Cuboid(center + new Vector3(-width * 0.2f, -height / 4f, 0), 5, height / 2, 2, false , Color.White);
             RightPost = new Cuboid(center + new Vector3(width * 0.2f, -height / 4f, 0), 5, height / 2, 2, false, Color.White);
             Net = new Cuboid(center + new Vector3(0, -height / 8f, 0), (int) (RightPost.Center.X - LeftPost.Center.X), height/4, 1, false, Color.Beige);
-            Bench1 = content.Load<Model>("bench");
+            _bench = content.Load<Model>("bench");
 
+        }
+
+        public void Draw(Camera camera)
+        {
+            DrawArena(_graphicsDevice, camera);
+            DrawLeftPost(_graphicsDevice, camera);
+            DrawRightPost(_graphicsDevice, camera);
+            DrawNet(_graphicsDevice, camera);
+            DrawBenches(camera);
+        }
+
+        private void DrawArena(GraphicsDevice device, Camera camera)
+        {
+            _effect.View = camera.ViewMatrix;
+            _effect.Projection = camera.ProjectionMatrix;
+            _effect.World = Matrix.Identity;
+            _effect.VertexColorEnabled = true;
+            foreach (var pass in _effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, Vertices, 0, Vertices.Length,
+                    Indices, 0, Indices.Length / 3, VertexPositionColor.VertexDeclaration);
+            }
+        }
+
+        private void DrawLeftPost(GraphicsDevice device, Camera camera)
+        {
+            _effect.View = camera.ViewMatrix;
+            _effect.Projection = camera.ProjectionMatrix;
+            _effect.World = Matrix.Identity;
+            _effect.VertexColorEnabled = true;
+            foreach (var pass in _effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, LeftPost.Vertices, 0, LeftPost.Vertices.Length,
+                    LeftPost.Indices, 0, LeftPost.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
+            }
+        }
+
+        private void DrawRightPost(GraphicsDevice device, Camera camera)
+        {
+            _effect.View = camera.ViewMatrix;
+            _effect.Projection = camera.ProjectionMatrix;
+            _effect.World = Matrix.Identity;
+            _effect.VertexColorEnabled = true;
+            foreach (var pass in _effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, RightPost.Vertices, 0,
+                    RightPost.Vertices.Length,
+                    RightPost.Indices, 0, RightPost.Indices.Length/3, VertexPositionColor.VertexDeclaration);
+            }
+        }
+
+        private void DrawNet(GraphicsDevice device, Camera camera)
+        {
+            _effect.View = camera.ViewMatrix;
+            _effect.Projection = camera.ProjectionMatrix;
+            _effect.World = Matrix.Identity;
+            _effect.VertexColorEnabled = true;
+            foreach (var pass in _effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                device.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, Net.Vertices, 0, Net.Vertices.Length,
+                    Net.Indices, 0, Net.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
+            }
+        }
+
+        private void DrawBenches(Camera camera)
+        {
+            var modelTransforms = new Matrix[_bench.Bones.Count];
+            var worldMatrix = Matrix.CreateScale(0.1f, 0.1f, 0.1f) * Matrix.CreateTranslation(new Vector3(-Width / 3f, -Height / 2f, 0));
+            _bench.CopyAbsoluteBoneTransformsTo(modelTransforms);
+            foreach (var mesh in _bench.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = modelTransforms[mesh.ParentBone.Index] * worldMatrix;
+                    effect.View = camera.ViewMatrix;
+                    effect.Projection = camera.ProjectionMatrix;
+                }
+                mesh.Draw();
+            }
+            worldMatrix = Matrix.CreateScale(0.1f, 0.1f, 0.1f) * Matrix.CreateTranslation(new Vector3(Width / 3f, -Height / 2f, 0));
+            foreach (var mesh in _bench.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = modelTransforms[mesh.ParentBone.Index] * worldMatrix;
+                }
+                mesh.Draw();
+            }
         }
     }
 }
--- gk3d/gk3d/Game.cs
+++ gk3d/gk3d/Game.cs
@@ -10,7 +10,6 @@ namespace gk3d
     public class Game : Microsoft.Xna.Framework.Game
     {
         private readonly GraphicsDeviceManager _graphics;
-        private BasicEffect _effect;
         private Arena _arena;
         private Camera _camera;
 
@@ -42,9 +41,8 @@ namespace gk3d
         /// </summary>
         protected override void LoadContent()
         {
-            _arena = new Arena(Content, new Vector3(0, 0, 0), 200, 50, 400, Color.Yellow);
+            _arena = new Arena(Content, GraphicsDevice, new Vector3(0, 0, 0), 200, 50, 400, Color.Yellow);
             _camera = new Camera(GraphicsDevice.Viewport);
-            _effect = new BasicEffect(GraphicsDevice);
         }
 
         /// <summary>
@@ -68,87 +66,9 @@ namespace gk3d
         {
             GraphicsDevice.Clear(Color.Black);
             var rs = new RasterizerState {FillMode = FillMode.WireFrame};
-            //GraphicsDevice.RasterizerState = rs;
-            DrawArena();
-            DrawLeftPost();
-            DrawRightPost();
-            DrawNet();
-            var modelTransforms = new Matrix[_arena.Bench1.Bones.Count];
-            var worldMatrix = Matrix.CreateScale(0.1f, 0.1f, 0.1f) * Matrix.CreateTranslation(new Vector3(-_arena.Width / 3f, -_arena.Height / 2f, 0));
-            _arena.Bench1.CopyAbsoluteBoneTransformsTo(modelTransforms);
-            foreach (var mesh in _arena.Bench1.Meshes)
-            {
-                foreach (BasicEffect effect in mesh.Effects)
-                {
-                    effect.World = modelTransforms[mesh.ParentBone.Index]*worldMatrix;
-                    effect.View = _camera.ViewMatrix;
-                    effect.Projection = _camera.ProjectionMatrix;
-                }
-                mesh.Draw();
-            }
+            GraphicsDevice.RasterizerState = rs;
+            _arena.Draw(_camera);
             base.Draw(gameTime);
-        }
-
-        private void DrawArena()
-        {
-            _effect.View = _camera.ViewMatrix;
-            _effect.Projection = _camera.ProjectionMatrix;
-            _effect.World = Matrix.Identity;
-            _effect.VertexColorEnabled = true;
-            foreach (var pass in _effect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _arena.Vertices, 0, _arena.Vertices.Length,
-                    _arena.Indices, 0, _arena.Indices.Length/3, VertexPositionColor.VertexDeclaration);
-                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _arena.LeftPost.Vertices, 0, _arena.LeftPost.Vertices.Length,
-                    _arena.LeftPost.Indices, 0, _arena.LeftPost.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
-                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _arena.RightPost.Vertices, 0, _arena.RightPost.Vertices.Length,
-                    _arena.RightPost.Indices, 0, _arena.RightPost.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
-                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _arena.Net.Vertices, 0, _arena.Net.Vertices.Length,
-                    _arena.Net.Indices, 0, _arena.Net.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
-            }
-        }
-
-        private void DrawNet()
-        {
-            _effect.View = _camera.ViewMatrix;
-            _effect.Projection = _camera.ProjectionMatrix;
-            _effect.World = Matrix.Identity;
-            _effect.VertexColorEnabled = true;
-            foreach (var pass in _effect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _arena.Net.Vertices, 0, _arena.Net.Vertices.Length,
-                    _arena.Net.Indices, 0, _arena.Net.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
-            }
-        }
-
-        private void DrawLeftPost()
-        {
-            _effect.View = _camera.ViewMatrix;
-            _effect.Projection = _camera.ProjectionMatrix;
-            _effect.World = Matrix.Identity;
-            _effect.VertexColorEnabled = true;
-            foreach (var pass in _effect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _arena.LeftPost.Vertices, 0, _arena.LeftPost.Vertices.Length,
-                    _arena.LeftPost.Indices, 0, _arena.LeftPost.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
-            }
-        }
-
-        private void DrawRightPost()
-        {
-            _effect.View = _camera.ViewMatrix;
-            _effect.Projection = _camera.ProjectionMatrix;
-            _effect.World = Matrix.Identity;
-            _effect.VertexColorEnabled = true;
-            foreach (var pass in _effect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _arena.RightPost.Vertices, 0, _arena.RightPost.Vertices.Length,
-                    _arena.RightPost.Indices, 0, _arena.RightPost.Indices.Length / 3, VertexPositionColor.VertexDeclaration);
-            }
         }
     }
 }
